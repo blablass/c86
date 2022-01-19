3d2fdf94f3ece93fbd71486fcb649a63b3cfd26a
blablass <82516957+blablass@users.noreply.github.com>
2022-01-19 18:50:49 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -17,7 +17,6 @@ function playerupdate(){
         
     });
 }
-playerupdate()
  function blockupdate(newImg){
     fabric.Image.fromURL(newImg,function(Img){
         block_object = Img;  
@@ -30,4 +29,58 @@ playerupdate()
         canvas.add(block_object);
     });
  }
- blockupdate("trunk.jpg")
+window.addEventListener("keydown",my_keydown)
+function my_keydown(e){
+keypressed = e.keyCode;
+console.log(keypressed);
+if(e.shiftKey== true && keypressed == '80'){
+    block_width = block_width + 10;
+    block_height = block_height + 10;
+    document.getElementById("current_width").innerHTML = block_width;
+    document.getElementById("current_height").innerHTML = block_height;
+}
+if(e.shiftKey== true && keypressed == '77' && block_width>0){
+    block_width = block_width - 10;
+    block_height = block_height - 10;
+    document.getElementById("current_width").innerHTML = block_width;
+    document.getElementById("current_height").innerHTML = block_height;
+}
+if(keypressed =='37'){
+    left()
+}
+if(keypressed =='38'){
+    up()
+}
+if(keypressed =='39'){
+    right()
+}
+if(keypressed =='40'){
+    down()
+}
+if(keypressed =='67'){
+    blockupdate("cloud.jpg");
+}
+if(keypressed =='68'){
+    blockupdate("dark_green.png");
+}
+if(keypressed =='71'){
+    blockupdate("ground.png");
+}
+if(keypressed =='82'){
+    blockupdate("roof.jpg");
+}
+if(keypressed =='84'){
+    blockupdate("trunk.jpg");
+}
+if(keypressed =='85'){
+    blockupdate("unique.png");
+}
+if(keypressed =='87'){
+    blockupdate("wall.jpg");
+}
+if(keypressed =='89'){
+    blockupdate("yellow_wall.png");
+}
+}
+
+
